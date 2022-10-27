b614c4d593e9541725f09015f63774950389c3bd
HAPPY <1737575350@qq.com>
2022-10-27 17:45:37 +0800

    NIO，Buffer

---
+++ src/main/java/com/demo/nio/BasicBuffer.java
@@ -0,0 +1,36 @@
+package com.demo.nio;
+
+import java.nio.IntBuffer;
+
+/**
+ * @Describe:
+ * @Author: HAPPY
+ * @Date: 2022-10-27 16:23 星期四
+ **/
+public class BasicBuffer {
+    //理解buffer的flip()方法切换读写
+    //debug理解Buffer的是个属性capacity position limit mark
+    public static void main(String[] args) {
+        IntBuffer intBuffer = IntBuffer.allocate(5);
+        //intBuffer.put(2);
+        //intBuffer.put(4);
+        //intBuffer.put(6);
+        //intBuffer.put(8);
+        //intBuffer.put(10);
+    
+        for(int i = 0; i < intBuffer.capacity(); i++) {
+            intBuffer.put( i * 2);
+        }
+    
+        //intBuffer.position();
+        //intBuffer.mark();
+        //intBuffer.limit();
+        //intBuffer.capacity();
+        
+        intBuffer.flip();//读写切换 写buffer 为 读buffer
+        while (intBuffer.hasRemaining()) {
+            System.out.println(intBuffer.get());
+        }
+    }
+    
+}
---
+++ src/main/java/com/demo/nio/NioServer.java
@@ -0,0 +1,22 @@
+package com.demo.nio;
+
+/**
+ * @Describe:
+ * @Author: HAPPY
+ * @Date: 2022-10-27 15:39 星期四
+ **/
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ */
+@Component
+public class NioServer {
+    
+    
+    public static void main(String[] args) {
+        //SocketChannel
+        //ServerSocketChannel
+    }
+}
