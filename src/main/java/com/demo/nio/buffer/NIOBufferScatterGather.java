1868c6a9f7f20df816f4fc4c6e080fc6258141d9
HAPPY <1737575350@qq.com>
2022-10-31 17:25:50 +0800

    通过scattering()和gathering分别对buffer数组进行依次写入[分散],依次读入[聚合]

---
+++ src/main/java/com/demo/nio/buffer/NIOBufferScatterGather.java
@@ -0,0 +1,15 @@
+package com.demo.nio.buffer;
+
+/**
+ * @Describe: NIO 还支持通过多个Buffer (即Buffer 数组) 完成读写操作
+ * 通过scattering()和 gathering 处理
+ * Scattering: 将数据写入到buffer 时,可以采用buffer 数组,依次写入[分散]
+ * Gathering: 从buffer 读取数据时,可以采用buffer 数组,依次读[聚合]
+ * @Author: HAPPY
+ * @Date: 2022-10-31 16:51 星期一
+ **/
+public class NIOBufferScatterGather {
+    public static void main(String[] args) {
+    
+    }
+}
