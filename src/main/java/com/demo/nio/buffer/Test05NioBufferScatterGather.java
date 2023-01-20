d692ce0b932cc1d02463d153b04a411053e88bea
Happy <1737575350@qq.com>
2023-01-20 14:59:04 +0800

    修改

--- src/main/java/com/demo/nio/buffer/Test05NioBufferScatterGather.java
+++ src/main/java/com/demo/nio/buffer/Test05NioBufferScatterGather.java
@@ -13,57 +13,65 @@ import java.util.Arrays;
  * 通过scattering()和 gathering 处理
  * Scattering: 将数据写入到buffer 时,可以采用buffer 数组,依次写入[分散]
  * Gathering: 从buffer 读取数据时,可以采用buffer 数组,依次读[聚合]
+ *
+ * 例子:
+ * telnet 建立连接后,比较 send hello  和 send  helloabc 对position 和 limit的影响
+ *
  * @Author: HAPPY
  * @Date: 2022-10-31 16:51 星期一
  **/
 public class Test05NioBufferScatterGather {
-    public static void main(String[] args) {
-    
-        try {
-            //建立服务端SocketChannel并启动
-            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-            //建立Socket端口地址
-            InetSocketAddress inetSocketAddress = new InetSocketAddress(7777);
-            //绑定端口
-            serverSocketChannel.socket().bind(inetSocketAddress);
-            
-            //建立buffer[]数组
-            ByteBuffer[] byteBufferArr = new ByteBuffer[2];
-            byteBufferArr[0] = ByteBuffer.allocate(5);
-            byteBufferArr[1] = ByteBuffer.allocate(3);
-            
-            //等待客户链接
-            SocketChannel socketChannel = serverSocketChannel.accept();
-            
-            int byteMsg = 8;//假设从客户端接受8个字节
-            while (true) {
-                int byteRead = 0;
-                while (byteMsg < byteRead) {
-                    long l = socketChannel.read(byteBufferArr);
-                    byteRead += l;
-                    System.out.println("字节数" + byteRead);
-                    Arrays.asList(byteBufferArr).stream().map(byteBuffer ->
-                                    "position=" + byteBuffer.position() + "," +
-                                    "limit=" + byteBuffer.limit()).
-                            forEach(System.out::println);
-                }
+    public static void main(String[] args) throws IOException {
+        //使用ServerSocketChannel 和SocketChannel 网络
+        
+        //建立服务端SocketChannel并启动
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        //建立Socket端口地址
+        int port = 7777;
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(port);
+        //绑定端口
+        serverSocketChannel.socket().bind(inetSocketAddress);
+        System.out.println("服务建立...;端口:" + port);
+        
+        //建立buffer[]数组
+        ByteBuffer[] byteBufferArr = new ByteBuffer[2];
+        byteBufferArr[0] = ByteBuffer.allocate(5);
+        byteBufferArr[1] = ByteBuffer.allocate(3);
+        
+        //等待客户链接
+        System.out.println("等待客户链接");
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        System.out.println("客户已连接");
+        
+        int msgLength = 8;//假设从客户端接受8个字节
+        while (true) {//循环读取
+            int byteRead = 0;
+            while (byteRead < msgLength) {
+                long l = socketChannel.read(byteBufferArr);
+                byteRead += l;//累计读取字节数
+                System.out.println("字节数" + byteRead);
+                //使用流打印当前buffer的position和limit
+                Arrays.asList(byteBufferArr).stream().map(byteBuffer ->
+                                "position=" + byteBuffer.position() + "," +
+                                        "limit=" + byteBuffer.limit()).
+                        forEach(System.out::println);
             }
             
+            //将所有的buffer进行flip
+            Arrays.asList(byteBufferArr).forEach(buffer -> buffer.flip());
             
-            //反转buffer
-            //for (int i = 0; i < byteBufferArr.length; i++) {
-            //    byteBufferArr[i].flip();
-            //}
+            //将数据读出显示到客户端
+            long byteWrite = 0;
+            while (byteWrite < msgLength) {
+                socketChannel.write(byteBufferArr);
+                long l = socketChannel.write(byteBufferArr);
+                byteWrite += l;
+            }
             
-            //向客户端输出
+            //将所有的buffer进行clear
+            Arrays.asList(byteBufferArr).forEach(buffer -> buffer.clear());
             
-            
-            
-            
-    
-    
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("byteRead=" + byteRead + "  byteWrite=" + byteWrite + "  msgLength=" + msgLength);
         }
     }
 }
